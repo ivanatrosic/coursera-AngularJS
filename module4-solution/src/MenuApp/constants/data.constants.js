bdfebda2e6fc51852b93fcab7e8ee1e665524660
ivanam <ivana@corsera.com>
2016-10-05 15:43:44 +0200

    v2.0

--- module4-solution/src/MenuApp/constants/data.constants.js
+++
@@ -1,7 +0,0 @@
-(function(angular){
-  'use strict';
-
-  angular.module('data')
-  .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
-
-})(angular);
--- module4-solution/src/MenuApp/services/data.service.js
+++ module4-solution/src/MenuApp/services/data.service.js
@@ -4,21 +4,21 @@
   angular.module('data')
   .service('MenuDataService', MenuDataService);
 
-  MenuDataService.$inject = ['$http','ApiBasePath'];
-  function MenuDataService($http,ApiBasePath) {
+  MenuDataService.$inject = ['$http'];
+  function MenuDataService($http) {
     var service = this;
 
     service.getAllCategories = function() {
       return $http({
         method: "GET",
-        url: (ApiBasePath + "/categories.json")
+        url: ("https://davids-restaurant.herokuapp.com/categories.json")
       });
     };
 
     service.getItemsForCategory = function(categoryShortName) {
       return $http({
         method: "GET",
-        url: (ApiBasePath + "/menu_items.json"),
+        url: ("https://davids-restaurant.herokuapp.com/menu_items.json"),
         params: {
           category: categoryShortName
         }
